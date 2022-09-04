5a334cf7a0f51541a2039804d8fce0c075c06a80
Maxim <maximgrigorovua@gmail.com>
2022-09-04 18:41:36 +0300

    ls table view

--- src/main/java/com/knubisoft/FileManager/command/Ls.java
+++ src/main/java/com/knubisoft/FileManager/command/Ls.java
@@ -1,6 +1,7 @@
 package com.knubisoft.FileManager.command;
 
 import java.io.File;
+import java.util.Formatter;
 import java.util.List;
 
 public class Ls extends Command {
@@ -13,12 +14,28 @@ public class Ls extends Command {
     public String execute(List<String> args) {
         File file = context.getCurrentDirectory();
         File[] allFiles = file.listFiles();
+
         StringBuilder result = new StringBuilder();
+        buildHeaderTable(result);
+
         if (allFiles != null) {
-            for (File each: allFiles){
-                result.append(each.getName()).append(":").append(each.getUsableSpace()).append("\r\n");
+            for (File each : allFiles) {
+                result.append(String.format("| %-13s | %-13d |%n", each.getName(), each.getUsableSpace()));
             }
         }
+        buildFooterTable(result);
+
         return result.toString();
+    }
+
+    private void buildHeaderTable(StringBuilder result) {
+        result.append(String.format("+-------------------------------+%n"));
+        result.append(String.format("| File name     | Usable Space  |%n"));
+        result.append(String.format("+-------------------------------+%n"));
+
+    }
+
+    private void buildFooterTable(StringBuilder result) {
+        result.append(String.format("+-------------------------------+%n"));
     }
 }
