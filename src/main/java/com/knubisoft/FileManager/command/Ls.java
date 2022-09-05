46d5788da6632261cfe0a672480ebe5925de7b83
Maxim <maximgrigorovua@gmail.com>
2022-09-05 11:52:47 +0300

    ls table check r/w

--- src/main/java/com/knubisoft/FileManager/command/Ls.java
+++ src/main/java/com/knubisoft/FileManager/command/Ls.java
@@ -1,5 +1,7 @@
 package com.knubisoft.FileManager.command;
 
+import org.apache.commons.io.FilenameUtils;
+
 import java.io.File;
 import java.util.Formatter;
 import java.util.List;
@@ -19,23 +21,51 @@ public class Ls extends Command {
         buildHeaderTable(result);
 
         if (allFiles != null) {
-            for (File each : allFiles) {
-                result.append(String.format("| %-13s | %-13d |%n", each.getName(), each.getUsableSpace()));
-            }
+            buildMainTable(result, allFiles);
         }
+
         buildFooterTable(result);
 
         return result.toString();
     }
 
     private void buildHeaderTable(StringBuilder result) {
-        result.append(String.format("+-------------------------------+%n"));
-        result.append(String.format("| File name     | Usable Space  |%n"));
-        result.append(String.format("+-------------------------------+%n"));
+        result.append(String.format("+--------------------------------------------------------------+%n"));
+        result.append(String.format("| File name     | Usable Space   | Read   | Write  | Extension |%n"));
+        result.append(String.format("+--------------------------------------------------------------+%n"));
 
     }
 
+    private void buildMainTable(StringBuilder result, File[] allFiles) {
+        String format = "| %-13s | %-13d  | %-5s  | %-5s | %-10s |%n";
+
+
+        for (File each : allFiles) {
+            String read = canRead(each);
+            String write = canWrite(each);
+
+            result.append(String.format(format, each.getName(), each.getUsableSpace(),
+                   read, write, FilenameUtils.getExtension(each.getName())));
+        }
+    }
+
+    private String canWrite(File each) {
+        if (each.canWrite()) {
+            return "write";
+        }
+        return "- ";
+    }
+
+    private String canRead(File each) {
+        if (each.canRead()) {
+            return "read ";
+        }
+
+        return "- ";
+    }
+
+
     private void buildFooterTable(StringBuilder result) {
-        result.append(String.format("+-------------------------------+%n"));
+        result.append(String.format("+--------------------------------------------------------------+%n"));
     }
 }
